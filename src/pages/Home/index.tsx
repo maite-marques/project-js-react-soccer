158c1433038a1d251d6d8d387ea855a57274e33d
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-28 18:41:30 -0300

    chore: add get api player

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { getPlayerRequest } from '@store/modules/players/actions';
+
+import { api } from '@services';
 
 import { Carrousel } from '@components';
 
 import { Container } from './styles';
 
 const Home: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    api.get('atletas/1').then(response => {
+      dispatch(getPlayerRequest(response.data));
+    });
+  }, []);
+
   return (
     <Container>
       <h1>Home Tests</h1>
