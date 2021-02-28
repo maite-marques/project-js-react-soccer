6473d5f67ec4124ffd2048026556445c97e75770
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-28 13:23:39 -0300

    feat: add carrousel component

---
+++ src/components/Carrousel/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Carrousel: React.FC = () => {
+  return (
+    <Container>
+      <h1>Carrousel</h1>
+    </Container>
+  );
+};
+
+export default Carrousel;
