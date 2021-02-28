2d211aaa85c7dd48a838afc80ca4a8acee79c79e
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-28 13:28:50 -0300

    chore: add carrousel component in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { Carrousel } from '@components';
+
 import { Container } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <h1>Home Tests</h1>
+      <Carrousel />
     </Container>
   );
 };
