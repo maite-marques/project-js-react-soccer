14dc2bb3bc675581d5eb47a32fcf04b3306575d8
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-28 18:31:07 -0300

    chore: add store path setup

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,7 @@ module.exports = function override(config) {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@store': path.resolve(__dirname, 'src/store'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@routes': path.resolve(__dirname, 'src/routes'),
       '@services': path.resolve(__dirname, 'src/services'),
